a7fd7cf3bb058ab5c8297e1ea6967514621cf278
Sebastian Rizzuto <srizzuto@gmail.com>
2021-05-05 08:21:12 -0300

    add game rspec test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,18 +1,66 @@
-require_relative '../lib/player'
-require_relative '../lib/board'
 require_relative '../lib/game'
+require_relative '../lib/board'
+require_relative '../lib/player'
 
 module TicTacToe
+  describe Game do
+    let(:mockPlayer) { double(player1: 'Player1', player2: 'Player2') }
+    let(:game) { Game.new(mockPlayer) }
+    describe '.initialize' do
+      it 'return the name of the players' do
+        expect(game.player.player1).to eq 'Player1'
+      end
+      it 'raise error if there is no arguments' do
+        expect { Game.new }.to raise_error
+      end
+    end
+
+    describe '#human_move_to_coordinate' do
+      it 'return the coordinates' do
+        expect(game.human_move_to_coordinate('3')).to eq [0, 2]
+      end
+    end
+    describe '#check_if_draw' do
+      it 'return nil if there is numbers left' do
+        expect(game.check_if_draw).to eq nil
+      end
+    end
+
+    describe '#posible_winning' do
+      it 'return array with all the possibilities' do
+        wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        expect(game.posible_winning).to eq wins
+      end
+    end
+
+    describe '#check_if_winner' do
+      it 'false if all the values in the array are not the same' do
+        expect(game.check_if_winner).to eq false
+      end
+    end
+
+    describe '#play' do
+      it 'return nil' do
+        game.stub(:play) { nil unless game.check_if_winner }
+        expect(game.play).to eq nil
+      end
+
+      it 'raise erro without stub(:play)' do
+        expect { game.play }.to raise_error
+      end
+    end
+  end
+
   describe Player do
     describe '#initialize' do
       it 'save two plyers' do
-        player = Player.new('Safa', 'Moises')
-        expect(player.player1).to eq 'Safa'
+        player = Player.new('Player1', 'Player2')
+        expect(player.player1).to eq 'Player1'
       end
 
       it 'save two plyers' do
-        player = Player.new('Safa', 'Moises')
-        expect(player.player2).to eq 'Moises'
+        player = Player.new('Player1', 'Player2')
+        expect(player.player2).to eq 'Player2'
       end
 
       it 'thrown error if no arguments' do
@@ -21,41 +69,39 @@ module TicTacToe
     end
   end
 
-  module TicTacToe
-    describe Board do
-      let(:board) { Board.new }
-      let(:arr) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
-  
-      describe '.initialize' do
-        it 'return three nested arrys' do
-          expect(board.grid).to eq arr
-        end
+  describe Board do
+    let(:board) { Board.new }
+    let(:arr) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
+
+    describe '.initialize' do
+      it 'return three nested arrys' do
+        expect(board.grid).to eq arr
       end
-  
-      describe '#default_grid' do
-        it 'return three nested arrys' do
-          expect(board.default_grid).to eq arr
-        end
+    end
+
+    describe '#default_grid' do
+      it 'return three nested arrys' do
+        expect(board.default_grid).to eq arr
       end
-  
-      describe '#get_cell' do
-        it 'return number nine' do
-          expect(board.get_cell(2, 2)).to eq 9
-        end
-  
-        it 'raise error if not arguments' do
-          expect { board.get_cell }.to raise_error
-        end
+    end
+
+    describe '#get_cell' do
+      it 'return number nine' do
+        expect(board.get_cell(2, 2)).to eq 9
       end
-  
-      describe '#set_cell' do
-        it 'return and X' do
-          expect(board.set_cell(2, 2, 'X')).to eq 'X'
-        end
-  
-        it 'raise error if not arguments' do
-          expect { board.set_cell }.to raise_error
-        end
+
+      it 'raise error if not arguments' do
+        expect { board.get_cell }.to raise_error
+      end
+    end
+
+    describe '#set_cell' do
+      it 'return and X' do
+        expect(board.set_cell(2, 2, 'X')).to eq 'X'
+      end
+
+      it 'raise error if not arguments' do
+        expect { board.set_cell }.to raise_error
       end
     end
   end
